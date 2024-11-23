67023f00eb799a5c4a778cef3cdba9904d9d835c
hartpableo <hart.hpwebdev@gmail.com>
2024-11-23 23:46:27 +0800

    Fix

--- app/controllers/FinancialTrackerController.php
+++ app/controllers/FinancialTrackerController.php
@@ -19,14 +19,15 @@ class FinancialTrackerController
   public function addItem(): void
   {
     $assets = $_POST['assets'] ?? [];
+    $item_type = $_POST['type'];
     $assets = array_filter((array) $assets);
     if (!empty($assets) && !empty($_POST['type'])) {
-      $saved_items = $this->db->query('SELECT title FROM financial_items WHERE type = :type', ['type' => $_POST['type']])->findAll();
+      $saved_items = $this->db->query('SELECT title FROM financial_items WHERE type = :type', ['type' => $item_type])->findAll();
       $assets = array_diff($assets, array_column($saved_items, 'title'));
       foreach ($assets as $asset) {
         $this->db->query('INSERT INTO financial_items (title, type) VALUES (:title, :type)', [
           'title' => $asset,
-          'type' => $_POST['type']
+          'type' => $item_type
         ]);
       }
     }
