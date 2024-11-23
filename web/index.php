d3d832f19b4bfe87698ce242eb295bde4a170313
hartpableo <hart.hpwebdev@gmail.com>
2024-11-23 21:33:09 +0800

    fix: paths

--- web/index.php
+++ web/index.php
@@ -4,8 +4,6 @@ require_once __DIR__ . '/../core/config.php';
 
 session_start();
 
-var_dump((new \core\Helpers())->baseUrl());
-
 $path = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 $router = new core\Router();
