1caa7a73e8fc066a4513a90f4811f93c99ba5690
hartpableo <hart.hpwebdev@gmail.com>
2024-11-29 18:17:31 +0800

    Fix

--- app/views/home.php
+++ app/views/home.php
@@ -8,33 +8,31 @@ $helper->templatePart('header', ['title' => $args['title']]);
     <div class="container pt-5">
         <h1 class="sr-only"><?php echo $args['title']; ?></h1>
 
+        <div class="mb-5">
+            <div class="table-responsive" style="max-width: 500px;">
+                <table class="table align-middle">
+                    <tr>
+                        <th>Total Monthly Liabilities</th>
+                        <td><?php echo $financialTracker->getTotalMonthlyLiabilities(); ?></td>
+                    </tr>
+                    <tr>
+                        <th>Total Remaining Cash</th>
+                        <td><?php echo $financialTracker->getTotalRemainingCash(); ?></td>
+                    </tr>
+                    <tr>
+                        <th>Available weekly Budget</th>
+                        <td><?php echo $financialTracker->getAvailableWeeklyBudget(); ?></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
         <div class="row">
             <div class="col-12 col-md-6">
               <?php $helper->templatePart('forms/assets'); ?>
             </div>
             <div class="col-12 col-md-6">
               <?php $helper->templatePart('forms/liabilities'); ?>
-            </div>
-        </div>
-
-        <div class="mt-5">
-            <div class="table-responsive">
-                <table class="table align-middle">
-                    <thead>
-                    <tr>
-                        <th>Total Monthly Liabilities</th>
-                        <th>Total Remaining Cash</th>
-                        <th>Available weekly Budget</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td><?php echo $financialTracker->getTotalMonthlyLiabilities(); ?></td>
-                        <td><?php echo $financialTracker->getTotalRemainingCash(); ?></td>
-                        <td><?php echo $financialTracker->getAvailableWeeklyBudget(); ?></td>
-                    </tr>
-                    </tbody>
-                </table>
             </div>
         </div>
     </div>
